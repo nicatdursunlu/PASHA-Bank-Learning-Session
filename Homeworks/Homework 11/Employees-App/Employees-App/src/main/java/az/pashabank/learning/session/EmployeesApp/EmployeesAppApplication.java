7ebb546ab52fc158e77b242d7e18c64e37e0a81a
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2021-08-27 21:58:46 +0400

    add Lesson 12

---
+++ Homeworks/Homework 11/Employees-App/Employees-App/src/main/java/az/pashabank/learning/session/EmployeesApp/EmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package az.pashabank.learning.session.EmployeesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAppApplication.class, args);
+	}
+
+}
---
+++ Homeworks/Homework 11/Employees-App/Employees-App/src/test/java/az/pashabank/learning/session/EmployeesApp/EmployeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.pashabank.learning.session.EmployeesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
